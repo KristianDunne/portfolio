4dd89319ea1ff2fa7e8566387ab4111dc803f52b
Kristian Dunne <KristianDunne@icloud.com>
2021-02-06 16:27:39 +0000

    Fix the introduction text on mobile

--- components/feature/hero/index.js
+++ components/feature/hero/index.js
@@ -4,8 +4,8 @@ import { motion } from 'framer-motion';
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
-  align-items: center;
   padding: 0 40px;
   max-width: 2228px;
   margin: 40px auto;
@@ -13,13 +13,12 @@ const Container = styled.div`
 
 const HeroText = styled.div`
   flex: 1;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
 `;
 
-const Avatar = styled.div`
-  display: flex;
-  flex: 1;
-  justify-content: flex-end;
-`;
+const Avatar = styled.div``;
 
 const Title = styled.h2`
   font-style: normal;
@@ -33,58 +32,97 @@ const SubTitle = styled.h2`
   margin: 1rem 0;
 `;
 
+const IntroductionMobile = styled.h3`
+  font-size: 1.5rem;
+  margin: 1rem 0;
+  font-weight: 400;
+
+  @media only screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
 const Introduction = styled.h3`
   font-size: 1.5rem;
   margin: 1rem 0;
   font-weight: 400;
+  display: none;
+  @media only screen and (min-width: 768px) {
+    display: flex;
+  }
+`;
+
+const TopSection = styled.div`
+  display: flex;
+  flex: 1;
+`;
+
+const BottomSection = styled.div`
+  display: flex;
+  flex: 1;
+
+  @media only screen and (min-width: 768px) {
+    display: none;
+  }
 `;
 
 export default function Hero() {
   return (
     <Container>
-      <HeroText>
-        <Title>
-          Hey,
-          <motion.div
-            style={{
-              marginBottom: '-20px',
-              marginRight: '-45px',
-              paddingBottom: '20px',
-              paddingRight: '45px',
-              display: 'inline-block',
-              paddingLeft: '16px',
-            }}
-            animate={{ rotate: 60 }}
-            transition={{
-              yoyo: 3,
-              from: 0,
-              duration: 0.8,
-              ease: 'easeInOut',
-              type: 'tween',
-            }}>
-            👋
-          </motion.div>
-        </Title>
-        <SubTitle>I'm Kristian.</SubTitle>
-        <Introduction>
+      <TopSection>
+        <HeroText>
+          <div>
+            <Title>
+              Hey,
+              <motion.div
+                style={{
+                  marginBottom: '-20px',
+                  marginRight: '-45px',
+                  paddingBottom: '20px',
+                  paddingRight: '45px',
+                  display: 'inline-block',
+                  paddingLeft: '16px',
+                }}
+                animate={{ rotate: 60 }}
+                transition={{
+                  yoyo: 3,
+                  from: 0,
+                  duration: 0.8,
+                  ease: 'easeInOut',
+                  type: 'tween',
+                }}>
+                👋
+              </motion.div>
+            </Title>
+          </div>
+          <div>
+            <SubTitle>I'm Kristian.</SubTitle>
+            <Introduction>
+              Frontend developer who loves bringing great user experiences to life with code.
+            </Introduction>
+          </div>
+        </HeroText>
+        <Avatar>
+          <Image
+            className="avatar"
+            src="/me.jpg"
+            width={170}
+            height={170}
+            alt="My Avatar"
+            role="presentation"
+          />
+          <style jsx global>{`
+            .avatar {
+              border-radius: 50%;
+            }
+          `}</style>
+        </Avatar>
+      </TopSection>
+      <BottomSection>
+        <IntroductionMobile>
           Frontend developer who loves bringing great user experiences to life with code.
-        </Introduction>
-      </HeroText>
-      <Avatar>
-        <Image
-          className="avatar"
-          src="/me.jpg"
-          width={170}
-          height={170}
-          alt="My Avatar"
-          role="presentation"
-        />
-        <style jsx global>{`
-          .avatar {
-            border-radius: 50%;
-          }
-        `}</style>
-      </Avatar>
+        </IntroductionMobile>
+      </BottomSection>
     </Container>
   );
 }
